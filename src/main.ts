451023dc52a505c337e673bdaecbd6c4353faa95
Fouad <m8fouad@gmail.com>
2021-08-14 04:16:09 +0200

    helm support

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import {Helm} from "./helm";
+
+async function main(): Promise<any> {
+
+    const helm = new Helm();
+
+    let releases = await helm.getReleases();
+    console.log(releases);
+
+    const release = releases[0];
+    console.log(helm.getChartName(release.chart));
+    console.log(helm.getChatVersion(release.chart));
+
+    let history = await helm.getReleaseHistory(release.name);
+    console.log(history);
+    console.log(await helm.getDiff(release.name, history[history.length -3].revision, history[history.length -2].revision));
+    return 0;
+}
+
+
+main().then();
